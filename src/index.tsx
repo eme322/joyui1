341f99762a8fdfe7752ee74d63f8377a3a9a1b46
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-03-07 13:32:29 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { StyledEngineProvider, CssVarsProvider } from '@mui/joy/styles';
-import Demo from './Demo';
+import Demo from './Demo'; 
 //import Demo from './Demo2';
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
