d6e193bb3b0742a290471fa0d305f526e2388568
Emely Lopez <130020311+eme322@users.noreply.github.com>
2024-03-25 22:03:00 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { StyledEngineProvider, CssVarsProvider } from '@mui/joy/styles';
 import Demo from './Demo'; 
-import Interface from './component2'; // Import Interface from component2.tsx
 
 import Demo2 from './Demo2';
 
@@ -20,7 +19,7 @@ ReactDOM.createRoot(document.querySelector("#root")!).render(
 
     //    <StepsSlider />
         
-      <Interface title="Example" buttonText="Click me" imageUrl="example.jpg" onClick={() => console.log("Clicked")} />
+
       </CssVarsProvider>
     </StyledEngineProvider>
   </React.StrictMode>
